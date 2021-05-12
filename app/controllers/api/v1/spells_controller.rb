6d10401f5ff069cc5e301c61ec58f472c8e51bf1
ohletamyleta <ohletamyleta@gmail.com>
2021-05-12 15:04:44 -0400

    configure serializers and controllers

--- app/controllers/api/v1/spells_controller.rb
+++ app/controllers/api/v1/spells_controller.rb
@@ -1,4 +1,9 @@
-class SpellsController < ApplicationController
+class Api::V1::SpellsController < ApplicationController
+
+    def index
+    spells = Spell.all 
+    render json: SpellSerializer.new(spells)
+  end
 
   
 end
