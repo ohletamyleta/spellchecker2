60576c2728dc8bb9363da8a8f8a86e7ae677ddf6
ohletamyleta <ohletamyleta@gmail.com>
2021-05-13 14:03:12 -0400

    link user login/signup to front end

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+class SessionsController < ApplicationController
+
 def create
     @user = User.find_by(username: session_params[:username])
   
@@ -38,4 +40,5 @@ private
 def session_params
       params.require(:user).permit(:username, :password)
 end
+
 end
