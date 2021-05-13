1af3c652887162631ca1c86326ba2b89fdbd8709
ohletamyleta <ohletamyleta@gmail.com>
2021-05-13 12:55:24 -0400

    build user routes and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 4 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [:create, :show, :index]
   
-  resources :spellbooks
-  resources :caster_classes
     namespace :api do 
       namespace :v1 do 
 
         resources :spells
         resources :caster_classes
+
+
 
       end
     end
